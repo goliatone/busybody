94c01cc7f022dd17e8f0c8788d51d91b2e9be943
goliatone <burgosemi@gmail.com>
2018-12-21 01:38:15 +0100

    rename example

--- example/rpc-client.js
+++ example/rpc-client.js
@@ -13,21 +13,6 @@ const anon = busybody.createNode(config, function(err) {
     console.log('somenode online!');
 });
 
-//-e /tmp/kiko4.sock -a publish -n 'node1:event' -m '{ "msg": "busybody_is_easy" }'
-anon.publish('node1:event', { msg: 'busybody_is_easy' }, function(err, message) {
-    if (err) {
-        return onError(err);
-    }
-    console.log('node1:event response message:', message); // -> 'ilovenodejs'
-});
-
-anon.publish('node1:ping', new Date(), (err, message) => {
-    if (err) {
-        return onError(err);
-    }
-    console.log('publish node1:ping response message:', message);
-});
-
 setTimeout(_ => {
     console.log('anon broadcast @event:');
     // anon.broadcast('broadcast_eve', { from: 'me' });
@@ -40,15 +25,9 @@ setTimeout(_ => {
 }, 1000);
 
 
-anon.on('broadcast_eve', (message, res) => {
-    console.log('On broadcast @anon event:', message);
-    console.log('response:', res);
-});
-
 function onError(e) {
     console.error(e);
 }
-
 
 const inter = repl.start({ prompt: '> ' });
 initializeContext();
